d773a1b9bba457ba54f05581affbd5fb1e10eaf1
Francisco <javiermartincuenca@gmail.com>
2017-10-31 02:44:16 +0100

    Animals list added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {ANIMALES} from '../../data/data.animales';
+import {Animal} from '../../interfaces/animal-interface';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales:any[]=[];
 
+  constructor(public navCtrl: NavController) {
+    this.animales=ANIMALES.splice(0);
   }
 
 }
